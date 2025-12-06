76dd3ce5d7fcb90c1062770a4dc80ab184988529
Robert MacWha <github@macwha.com>
2025-12-06 00:49:20 -0500

    downgrade to wasmi 0.50

--- crates/wasmi-plugin-hdk/src/compile.rs
+++ crates/wasmi-plugin-hdk/src/compile.rs
@@ -6,7 +6,7 @@ pub fn compile_plugin(wasm_bytes: Vec<u8>) -> Result<(Engine, Module), wasmi::Er
     // https://github.com/wasmi-labs/wasmi/issues/1647
     // TODO: Switch to lazy execution, seems significantly faster.
     config.compilation_mode(wasmi::CompilationMode::Eager);
-    config.set_max_cached_stacks(10);
+    config.set_cached_stacks(10);
     let engine = Engine::new(&config);
     let module = Module::new(&engine, wasm_bytes)?;
 
